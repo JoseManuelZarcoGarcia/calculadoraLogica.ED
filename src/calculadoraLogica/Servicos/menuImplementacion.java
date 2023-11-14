71115c2fd22b07ff5c351baab1cb7db3b35477c4
DESKTOP-A37MTN2\csi22-jzarcia <jose.manuel.zarco.garcia@gmail.com>
2023-11-14 12:13:57 +0100

    no message

--- src/calculadoraLogica/Servicos/menuImplementacion.java
+++ src/calculadoraLogica/Servicos/menuImplementacion.java
@@ -11,12 +11,9 @@ public class menuImplementacion implements menuInterfaz{
 		int seleccionUsuario;
 		
 		System.out.println("################################");
-		System.out.println("0. Cerrar la aplicacion");
-		System.out.println("1. Registro de paciente");
-		System.out.println("2. Ingreso de paciente");
-		System.out.println("3. Alta de paciente");
-		System.out.println("4. Resumen de ingresos y altas");
-		System.out.println("5. Historial del paciente");
+		System.out.println("1. Cerrar la aplicacion");
+		System.out.println("2. Igualdad");
+		System.out.println("3. Desigualdad");
 		System.out.println("################################");
 		System.out.println("Elija una opcion; ");
 		
